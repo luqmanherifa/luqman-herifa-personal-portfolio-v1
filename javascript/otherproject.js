b460f79d1291ad9c66a4330cc8688042380f198c
luqmanherifa <99471792+luqmanherifa@users.noreply.github.com>
2022-11-24 11:11:47 +0700

    Update

--- javascript/otherproject.js
+++ javascript/otherproject.js
@@ -1,30 +1,21 @@
+const otherProjectHidden = document.getElementsByClassName('other-project-hidden');
 var triggerHidden = document.getElementById("triggerHidden");
 var btnShow = document.getElementById("btnShow");
-var otherProjectHidden1 = document.getElementById("otherProjectHidden1");
-var otherProjectHidden2 = document.getElementById("otherProjectHidden2");
-var otherProjectHidden3 = document.getElementById("otherProjectHidden3");
-var otherProjectHidden4 = document.getElementById("otherProjectHidden4");
-var otherProjectHidden5 = document.getElementById("otherProjectHidden5");
-var otherProjectHidden6 = document.getElementById("otherProjectHidden6");
 
-function functionShow() {
-  if (triggerHidden.style.display === "none") {
-    triggerHidden.style.display = "inline";
-    btnShow.innerHTML = "Show More";
-    otherProjectHidden1.style.display = "none";
-    otherProjectHidden2.style.display = "none";
-    otherProjectHidden3.style.display = "none";
-    otherProjectHidden4.style.display = "none";
-    otherProjectHidden5.style.display = "none";
-    otherProjectHidden6.style.display = "none";
-  } else {
-    triggerHidden.style.display = "none";
-    btnShow.innerHTML = "Show Less";
-    otherProjectHidden1.style.display = "inline";
-    otherProjectHidden2.style.display = "inline";
-    otherProjectHidden3.style.display = "inline";
-    otherProjectHidden4.style.display = "inline";
-    otherProjectHidden5.style.display = "inline";
-    otherProjectHidden6.style.display = "inline";
+  btnShow.addEventListener("click", functionShow);
+
+  function functionShow() {
+    if (triggerHidden.style.display === "none") {
+      triggerHidden.style.display = "inline";
+      btnShow.innerHTML = "Show More";
+      for(var i = 0; i < otherProjectHidden.length; i++) {
+        otherProjectHidden[i].style.display = "none";
+      }
+    } else {
+      triggerHidden.style.display = "none";
+      btnShow.innerHTML = "Show Less";
+      for(var i = 0; i < otherProjectHidden.length; i++) {
+        otherProjectHidden[i].style.display = "inline";
+      }
+    }
   }
-}
--- javascript/top.js
+++ javascript/top.js
@@ -2,6 +2,8 @@ let btnTop = document.getElementById("btnTop");
 
 window.onscroll = function() {functionScroll()};
 
+btnTop.addEventListener("click", functionTop);
+
 function functionScroll() {
   if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
     btnTop.style.display = "block";
